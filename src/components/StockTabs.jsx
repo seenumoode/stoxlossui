60e7650902e6cead0cd1b28c3a895705624d9202
Usha Sree Ramavath <seenu.moode@gmail.com>
2025-05-27 10:44:25 +0530

    Added fix for selected stocks

--- src/components/StockTabs.jsx
+++ src/components/StockTabs.jsx
@@ -80,6 +80,28 @@ const StockTabs = () => {
       .then((data) => {
         setGainers(data.gainers);
         setLosers(data.losers);
+        const preSelected = data.losers.filter((stock) => {
+          if (
+            stock.data &&
+            Array.isArray(stock.data) &&
+            stock.data.length >= 3 &&
+            typeof stock.high === "number" &&
+            typeof stock.data[2].close === "number"
+          ) {
+            const highGreaterThanClose = stock.high > stock.data[2].close;
+            const allNegativePercentageChange =
+              typeof stock.data[0].percentageChange === "number" &&
+              typeof stock.data[1].percentageChange === "number" &&
+              typeof stock.data[2].percentageChange === "number" &&
+              stock.data[0].percentageChange < 0 &&
+              stock.data[1].percentageChange < 0 &&
+              stock.data[2].percentageChange < 0;
+            return highGreaterThanClose && allNegativePercentageChange;
+          }
+          return false;
+        });
+
+        setSelectedStocks(preSelected);
       })
       .catch((error) => {
         console.error("Error:", error);
